607eaf58ee47a3bda6d2fc577cd0b6281b8d69c9
DevanshChhabra <devanshchhabra2020@gmail.com>
2025-07-15 16:32:04 +0530

    our team section mobile and new header on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
-
-
 import type { Metadata } from "next";
 import "./globals.css";
-
 import Header from './components/Header';
-
 import Footer from './components/Footer';
-
-// Use different weights based on your needs
 
 export const metadata: Metadata = {
   title: "VVWorx Future Forward Marketing",
@@ -20,21 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-
     <html lang="en">   
-        <head>
+      <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
       </head>  
-            <body suppressHydrationWarning>
-
-
-        {/* Wrap the entire page with the children */}
-        {/* <Header /> */}
+      <body suppressHydrationWarning>
+        <Header />
         <main>{children}</main>
-
-
         <Footer />
-
       </body>
     </html>
   );
